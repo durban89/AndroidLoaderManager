d7cecc54b89591e68ae9cded21bce199e6f896a3
dapeng126 <zhangdapeng89@126.com>
2016-09-27 08:28:49 +0800

    dbhelper and content provider

---
+++ app/src/main/java/com/gowhich/androidloadermanager/db/DBHelper.java
@@ -0,0 +1,20 @@
+package com.gowhich.androidloadermanager.db;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by durban126 on 16/9/27.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}

a436c45f04f62a2a309d662fb4cb6e50c9afa03e
dapeng126 <zhangdapeng89@126.com>
2016-09-28 09:07:28 +0800

    添加 DBhelper

--- app/src/main/java/com/gowhich/androidloadermanager/db/DBHelper.java
+++ app/src/main/java/com/gowhich/androidloadermanager/db/DBHelper.java
@@ -1,5 +1,6 @@
 package com.gowhich.androidloadermanager.db;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -8,9 +9,17 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class DBHelper extends SQLiteOpenHelper {
+
+    private static String name = "person.db";
+    private static int version = 1;
+
+    public DBHelper(Context context) {
+        super(context, name, null, version);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String sql = "create table person (pid integer primary key autoincrement, name varchar(45), age varchar(45))";
     }
 
     @Override
--- app/src/main/java/com/gowhich/androidloadermanager/service/PersonContentProvider.java
+++ app/src/main/java/com/gowhich/androidloadermanager/service/PersonContentProvider.java
@@ -2,29 +2,39 @@ package com.gowhich.androidloadermanager.service;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 
 /**
- * Created by durban126 on 16/9/27.
+ * Created by durban126 on 16/9/28.
  */
 
-public class StudentContentProvider extends ContentProvider {
+public class PersonContentProvider extends ContentProvider {
+
+    private final static UriMatcher URI_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
+    private final int PERSON = 1;
+    private final int PERSONS = 2;
+
     @Override
     public boolean onCreate() {
         return false;
     }
 
+    @Nullable
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
         return null;
     }
 
+    @Nullable
     @Override
     public String getType(Uri uri) {
         return null;
     }
 
+    @Nullable
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         return null;

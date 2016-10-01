2daa73ea951efb625bbaea882af8b0f0b725d03b
dapeng126 <zhangdapeng89@126.com>
2016-10-01 21:41:40 +0800

    修改初始化方法

--- app/src/main/java/com/gowhich/androidloadermanager/db/DBHelper.java
+++ app/src/main/java/com/gowhich/androidloadermanager/db/DBHelper.java
@@ -1,5 +1,6 @@
 package com.gowhich.androidloadermanager.db;
 
+import android.content.ContentProvider;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -17,9 +18,14 @@ public class DBHelper extends SQLiteOpenHelper {
         super(context, name, null, version);
     }
 
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         String sql = "create table person (pid integer primary key autoincrement, name varchar(45), age varchar(45))";
+        db.execSQL(sql);
     }
 
     @Override
